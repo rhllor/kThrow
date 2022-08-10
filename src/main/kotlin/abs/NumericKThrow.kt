065ae70cbcadf8c7a3501363dae3b21066a62b8f
rhllor <ale.mule@gmail.com>
2022-08-10 19:23:14 +0200

    feat: implement interface for Numeric Value and String

---
+++ src/main/kotlin/abs/NumericKThrow.kt
@@ -0,0 +1,21 @@
+package com.github.rhllor.abs
+
+interface NumericKThrow<T> {
+    fun ifZero(message: String = "Value should not be 0"): NumericKThrow<T>
+
+    fun ifNotZero(message: String = "Value should be 0"): NumericKThrow<T>
+
+    fun ifPositive(message: String = "Value should not be greater than 0"): NumericKThrow<T>
+
+    fun ifNegative(message: String = "Value should not be less than 0"): NumericKThrow<T>
+
+    fun ifEqual(valueToCompare: T, message: String = "Value should not be equal than {0}"): NumericKThrow<T>
+
+    fun ifNotEqual(valueToCompare: T, message: String = "Value should be equal than {0}"): NumericKThrow<T>
+
+    fun ifLessThan(valueToCompare: T, message: String = "Value should not be less than {0}"): NumericKThrow<T>
+
+    fun ifGreaterThan(valueToCompare: T, message: String = "Value should not be greater than {0}"): NumericKThrow<T>
+
+    fun ifOutOfRange(fromValue: T, toValue: T, message: String = "Value should be between {0} and {1}"): NumericKThrow<T>
+}
---
+++ src/main/kotlin/abs/StringKThrow.kt
@@ -0,0 +1,31 @@
+package com.github.rhllor.abs
+
+interface StringKThrow {
+    fun ifEmpty(message: String = "String should not be empty"): StringKThrow
+
+    fun ifWhiteSpace(message: String = "String should not be white space only"): StringKThrow
+
+    fun ifLengthEquals(length: Int, message: String = "String length should not be equal to {0}"): StringKThrow
+
+    fun ifLengthNotEquals(length: Int, message: String = "String length should be equal to {0}"): StringKThrow
+
+    fun ifShorterThan(length: Int, message: String = "String should not be shorter than {0} characters"): StringKThrow
+
+    fun ifLongerThan(length: Int, message: String = "String should not be longer than {0} characters"): StringKThrow
+
+    fun ifEquals(valueToCompare: String, ignoreCase: Boolean = false, message: String = "String should not be equal to '{0}'"): StringKThrow
+
+    fun ifNotEquals(valueToCompare: String, ignoreCase: Boolean = false, message: String = "String should be equal to '{0}'"): StringKThrow
+
+    fun ifContains(valueToSearch: String, ignoreCase: Boolean = false, message: String = "String should not contain '{0}'"): StringKThrow
+
+    fun ifNotContains(valueToSearch: String, ignoreCase: Boolean = false, message: String = "String should contain '{0}'"): StringKThrow
+
+    fun ifStartsWith(valueToCompare: String, ignoreCase: Boolean = false, message: String = "String should not start with '{0}'"): StringKThrow
+
+    fun ifNotStartsWith(valueToCompare: String, ignoreCase: Boolean = false, message: String = "String should start with '{0}'"): StringKThrow
+
+    fun ifEndsWith(valueToCompare: String, ignoreCase: Boolean = false, message: String = "String should not end with '{0}'"): StringKThrow
+
+    fun ifNotEndsWith(valueToCompare: String, ignoreCase: Boolean = false, message: String = "String should end with '{0}"): StringKThrow
+}
